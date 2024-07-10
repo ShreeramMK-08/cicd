19e27bd7d317f71672f194c283bb9e5551648243
ShreeramMK-08 <145558168+ShreeramMK-08@users.noreply.github.com>
2024-07-10 21:53:31 +0530

    Add files via upload

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,50 @@
+pipeline {
+    agent any
+    environment {
+        AWS_DEFAULT_REGION = 'ap-south-1'
+        CLUSTER_NAME = 'CICD'
+        ECR_REPO_URI = '019848826665.dkr.ecr.ap-south-1.amazonaws.com/cicd'
+        IMAGE_TAG = 'latest'
+    }
+    stages {
+        stage('Clone repository') {
+            steps {
+                git 'https://github.com/your-username/your-repository.git'
+            }
+        }
+        stage('Build Docker image') {
+            steps {
+                script {
+                    docker.build("${ECR_REPO_URI}:${IMAGE_TAG}")
+                }
+            }
+        }
+        stage('Login to ECR') {
+            steps {
+                script {
+                    sh 'aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $ECR_REPO_URI'
+                }
+            }
+        }
+        stage('Push Docker image to ECR') {
+            steps {
+                script {
+                    docker.image("${ECR_REPO_URI}:${IMAGE_TAG}").push()
+                }
+            }
+        }
+        stage('Configure kubectl') {
+            steps {
+                sh 'aws eks update-kubeconfig --name $CLUSTER_NAME'
+            }
+        }
+        stage('Deploy to EKS') {
+            steps {
+                script {
+                    sh 'sed -i "s|<IMAGE>|${ECR_REPO_URI}:${IMAGE_TAG}|g" k8s/deployment.yaml'
+                    sh 'kubectl apply -f k8s/deployment.yaml'
+                }
+            }
+        }
+    }
+}
